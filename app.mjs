99e909b518c89050b7607415eadc3d3923404059
Beasley <ebeasley1@una.edu>
2025-08-28 13:06:01 -0500

    i have no idea what we are doing

--- app.mjs
+++ app.mjs
@@ -1,12 +1,14 @@
 import express from 'express'
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const app = express()
-
 const PORT = process.env.PORT || 3000;
+// const path = require('path');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const path = require('path');
-
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello Express from Render <a href="/erika">erika</a>')
@@ -18,8 +20,7 @@ app.get('/', (req, res) => {
 
 app.get('/erika', (req, res) => {
   //res.send('ERIKA. <a href="/">home</a>')
-
-  res.sendFile('erika.html')
+  res.send(join(__dirname, 'public', 'erika.html'))
 })
 
 app.listen(3000)
